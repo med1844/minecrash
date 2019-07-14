16fbaa76fcf31b5d2c692049c28a87305681de7c
medioqrity <medioqrity@gmail.com>
2019-07-14 22:44:59 +0800

    Changed MainEngine to a new thread

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -5,6 +5,6 @@ import engine.MainEngine;
 public class Main {
     public static void main(String[] args) {
         MainEngine engine = new MainEngine(1280, 720, "minecrash", true);
-        engine.run();
+        engine.start();
     }
 }
